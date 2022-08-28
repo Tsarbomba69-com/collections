ebc7c30510f19d0e3e125b61ae6fb4c0f24fa9e8
“Tsarbomba69-com” <hendrickdias66@gmail.com>
2022-08-28 17:00:02 +0100

    enqueue, dequeue, clear, append, reverse, print

--- vec.c
+++ vec.c
@@ -13,12 +13,11 @@ typedef struct Node
 typedef struct Vec
 {
     u_int len;
-    u_int cap;
-    Node *root;
+    Node *head;
 } Vec;
 
 //--------------------------------------
-// TODO: Move to a different file       
+// TODO: Move to a different file
 typedef void *Ok;
 
 typedef struct Result
@@ -41,45 +40,164 @@ void push(Vec *vec, void *data)
     Node *el = new_node(data);
     assert(el != NULL && vec != NULL);
 
-    if (vec->root == NULL)
-        vec->root = el;
+    if (vec->head == NULL)
+        vec->head = el;
     else
     {
-        el->next = vec->root;
-        vec->root = el;
+        el->next = vec->head;
+        vec->head = el;
     }
 
     vec->len += 1;
 }
 
+void enqueue(Vec *vec, void *data)
+{
+    Node *el = new_node(data);
+    assert(el != NULL && vec != NULL);
+
+    if (vec->head == NULL)
+        vec->head = el;
+    else
+    {
+        Node *cursor = vec->head;
+        while (cursor != NULL)
+        {
+            if (cursor->next == NULL)
+            {
+                cursor->next = el;
+                el->next = NULL;
+            }
+
+            cursor = cursor->next;
+        }
+    }
+    vec->len++;
+}
+
+void *dequeue(Vec *vec)
+{
+    assert(vec != NULL && vec->len > 0);
+
+    Node *cursor = vec->head;
+    while (cursor != NULL)
+    {
+        if (cursor->next == NULL)
+        {
+            void *result = cursor->data;
+            free(cursor);
+            vec->len--;
+            return result;
+        }
+
+        cursor = cursor->next;
+    }
+}
+
 void *pop(Vec *vec)
 {
     assert(vec != NULL && vec->len > 0);
-    Node *tmp = vec->root;
-    vec->root = tmp->next;
+    Node *tmp = vec->head;
+    vec->head = tmp->next;
     void *result = tmp->data;
-    tmp->next = NULL;
     free(tmp);
     vec->len--;
     return result;
 }
 
+void reverse(Vec *vec)
+{
+    assert(vec != NULL);
+    Node *prev = NULL;
+    Node *current = vec->head;
+
+    while (current != NULL)
+    {
+        Node *next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    vec->head = prev;
+}
+
+void clear(Vec *vec)
+{
+    assert(vec != NULL);
+
+    Node *current = vec->head;
+
+    while (current != NULL)
+    {
+        Node *tmp = current;
+        current = current->next;
+        free(tmp);
+    }
+
+    vec->len = 0;
+}
+
+void print_vec(Vec *vec)
+{
+    assert(vec != NULL);
+    Node *tmp = vec->head;
+    printf("[");
+
+    while (tmp != NULL)
+    {
+        printf("%p, ", tmp->data == NULL ? 0x0 : tmp->data);
+        tmp = tmp->next;
+    }
+
+    puts("]");
+}
+
+void append(Vec *dest, Vec *other)
+{
+    assert(dest != NULL && other != NULL && other->len > 0);
+
+    while (other->len > 0)
+    {
+        enqueue(dest, pop(other));
+    }
+}
+
 Vec new_vec()
 {
-    Vec vec = {.len = 0, .cap = 10, .root = (Node *)malloc(10 * sizeof(Node))};
+    Vec vec = {.len = 0, .head = (Node *)malloc(10 * sizeof(Node))};
+    vec.head = NULL;
     return vec;
 }
 
 int main()
 {
     Vec a = new_vec();
-    printf("a = { \n\tlen: %i, \n\tcap: %i, \n\troot: %p \n}\n", a.len, a.cap, a.root);
+    Vec b = new_vec();
+    printf("a = { \n\tlen: %i, \n\thead: %p \n}\n", a.len, a.head);
     push(&a, 1);
     push(&a, 2);
     push(&a, 3);
+    printf("a = ");
+    print_vec(&a);
+    enqueue(&b, 4);
+    enqueue(&b, 5);
+    enqueue(&b, 6);
+    printf("q = ");
+    print_vec(&b);
+    append(&a, &b);
+    reverse(&a);
+    reverse(&a);
+    reverse(&a);
+    printf("vec = ");
+    print_vec(&a);
+    clear(&a);
     while (a.len > 0)
     {
         printf("v = %i\n", (int)pop(&a));
     }
+
+    push(&a, 9);
+    printf("head->data = %i\n", (int)a.head->data);
     return EXIT_SUCCESS;
 }
